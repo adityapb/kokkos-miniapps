e675b9710732c942f82d56f36809a288c3245ce6
Aditya Bhosale <adityapb1546@gmail.com>
2026-01-28 13:34:05 -0600

    Fix Q

--- lulesh-2.0/kokkos-charm/lulesh-comm.cc
+++ lulesh-2.0/kokkos-charm/lulesh-comm.cc
@@ -717,12 +717,13 @@ void DomainChare::processRemoteQ(int ref, int x, int y, int z, int xferFields, i
          Kokkos::View<Real_t*> &dest = fieldData[fi] ;
          if (dest.data() == nullptr)
             CkPrintf("processRemoteQ: dest fieldData[%d] is null\n", fi);
-         Copy2D(domain.commDataRecvView, 
+         Copy1D(domain.commDataRecvView, 
             offset + fi * cdata.size[0] * cdata.size[1],
-            cdata.src_stride[0], cdata.src_stride[1],
-            dest, fieldOffset[fi] + cdata.pmsg * maxPlaneComm, cdata.dst_stride[0], cdata.dst_stride[1],
-            cdata.size[0], cdata.size[1], commSpace
+            1,
+            dest, fieldOffset[fi], 1,
+            cdata.size[0] * cdata.size[1], commSpace
             );
+         fieldOffset[fi] += cdata.size[0] * cdata.size[1];
       }
    } else {
       for (Index_t fi=0 ; fi<xferFields; ++fi) {
@@ -731,10 +732,11 @@ void DomainChare::processRemoteQ(int ref, int x, int y, int z, int xferFields, i
             CkPrintf("processRemoteQ: dest fieldData[%d] is null\n", fi);
          Copy1D(domain.commDataRecvView, 
             offset + fi * cdata.size[0],
-            cdata.src_stride[0],
-            dest, fieldOffset[fi] + cdata.pmsg * maxPlaneComm, cdata.dst_stride[0],
+            1,
+            dest, fieldOffset[fi], 1,
             cdata.size[0], commSpace
             );
+         fieldOffset[fi] += cdata.size[0];
       }
    }
 }
