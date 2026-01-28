2ad9df2487c9a8c99309d4c198a1d22e16e0457b
Aditya Bhosale <adityapb1546@gmail.com>
2026-01-28 09:31:09 -0600

    Fix error

--- lulesh-2.0/kokkos-charm/lulesh-comm.cc
+++ lulesh-2.0/kokkos-charm/lulesh-comm.cc
@@ -764,9 +764,10 @@ void DomainChare::processRemoteMass(int ref, int x, int y, int z, int xferFields
          offsetX == 0 && ((offsetY == -1 || offsetY == 1) && offsetZ == 0)) {
       for (Index_t fi=0 ; fi<xferFields; ++fi) {
          Kokkos::View<Real_t*> dest = fieldData[fi] ;
-         CkPrintf("[DBG] processRemoteMass: 2D Add offset=%d size0=%d size1=%d, src_stride=%d %d, dst_stride=%d %d\n", 
+         CkPrintf("[DBG] processRemoteMass: 2D Add offset=%d size0=%d size1=%d, src_stride=%d %d, dst_stride=%d %d, dest_size=%d\n", 
             offset + fi * cdata.size[0] * cdata.size[1],
-            cdata.size[0], cdata.size[1], cdata.src_stride[0], cdata.src_stride[1], cdata.dst_stride[0], cdata.dst_stride[1]);
+            cdata.size[0], cdata.size[1], cdata.src_stride[0], cdata.src_stride[1], 
+            cdata.dst_stride[0], cdata.dst_stride[1], dest.size());
          Add2D(domain.commDataRecvView, 
                offset + fi * cdata.size[0] * cdata.size[1],
                cdata.src_stride[0], cdata.src_stride[1],
