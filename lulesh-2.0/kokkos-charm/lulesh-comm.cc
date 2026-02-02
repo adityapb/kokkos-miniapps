434a115be71b18d9141ee00d3602296d029616ab
Aditya Bhosale <adityapb1546@gmail.com>
2026-02-02 05:57:42 -0600

    Error checking

--- lulesh-2.0/kokkos-charm/lulesh-comm.cc
+++ lulesh-2.0/kokkos-charm/lulesh-comm.cc
@@ -570,8 +570,8 @@ void DomainChare::CommSend(Domain& domain, int msgType,
 
       if (((offsetX == -1 || offsetX == 1) && offsetY == 0 && offsetZ == 0) || 
          offsetX == 0 && ((offsetY == -1 || offsetY == 1) && offsetZ == 0)) {
-         CkPrintf("Packing Kernel: Performing 1D copy to offset %d, src_stride %d %d, dst_stride %d %d\n", 
-            offset, cdata.src_stride[0], cdata.src_stride[1], cdata.dst_stride[0], cdata.dst_stride[1]);
+         CkPrintf("Packing Kernel: Performing 1D copy to offset %d, src_stride %d %d, dst_stride %d %d, size %d %d\n", 
+            offset, cdata.src_stride[0], cdata.src_stride[1], cdata.dst_stride[0], cdata.dst_stride[1], cdata.size[0], cdata.size[1]);
          for (Index_t fi=0 ; fi<xferFields; ++fi) {
             //CkPrintf("2D copy offsetX=%d offsetY=%d offsetZ=%d\n", offsetX, offsetY, offsetZ);
             Kokkos::View<Real_t*> src = fieldData[fi] ;
@@ -586,8 +586,8 @@ void DomainChare::CommSend(Domain& domain, int msgType,
          //     commDataSendView size=%lu, send offset=%d\n", 
          //     offsetX, offsetY, offsetZ, cdata.offset, dx, dy, dz, domain.commDataSendView.size(), 
          //     offset);
-         CkPrintf("Packing Kernel: Performing 1D copy to offset %d, src_stride %d, dst_stride %d\n", 
-            offset, cdata.src_stride[0], cdata.dst_stride[0]);
+         CkPrintf("Packing Kernel: Performing 1D copy to offset %d, src_stride %d, dst_stride %d, size %d\n", 
+            offset, cdata.src_stride[0], cdata.dst_stride[0], cdata.size[0]);
          for (Index_t fi=0 ; fi<xferFields; ++fi) {
             Kokkos::View<Real_t*> src = fieldData[fi] ;
             Copy1D(src, cdata.offset, cdata.src_stride[0],
